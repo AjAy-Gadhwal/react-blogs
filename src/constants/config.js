d2846a7a9be6b297cf8871dab3107e847c48b013
AjAy-Gadhwal <ajaygadhwal1@gmail.com>
2023-08-19 13:24:20 +0530

    change server url

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,4 +1,4 @@
-const SERVER_URL = 'https://node-blogs-flax.vercel.app';
+const SERVER_URL = 'https://node-blogs-1e7dyt5st-ajay-gadhwal.vercel.app';
 // const SERVER_URL = 'http://localhost:3001';
 
 
